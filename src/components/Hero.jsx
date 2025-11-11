a5500712dc04f863bee1649698627e7616b19836
Ab Momin <mominurrahman10748@gmail.com>
2025-11-11 22:44:22 +0600

    banner image added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-
+import bgImg from '../assets/photo-1673530936001-9a62842746c7.avif';
 const Hero = () => {
     return (
         <div>
-            <div className="hero bg-base-200 min-h-screen">
-                <div className="hero-content text-center">
+            <div
+                className="hero  bg-cover bg-center h-screen"
+                style={{
+                    backgroundImage: `url(${bgImg})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    height: '100vh',
+                    }}
+                >
+                <div className="hero-content text-neutral-content text-center">
                     <div className="max-w-md">
-                    <h1 className="text-5xl font-bold">Hello there</h1>
-                    <p className="py-6">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">
                         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
                         quasi. In deleniti eaque aut repudiandae et a id nisi.
                     </p>
